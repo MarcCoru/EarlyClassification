681a1c81da35dab6ae9bc92199ac90ab29b9843a
Marc Rußwurm <marc.russwurm@tum.de>
2019-07-17 13:27:26 +0200

    setup experiment

--- runtimes.sh
+++ runtimes.sh
@@ -1,7 +1,10 @@
-samplesize=10
+for samplesize in 10 50 100 250 500 750 1000 2500 5000 7500 10000;
+do
 
-echo $PWD
+echo $(date) $PWD
 mkdir -p $PWD/Results_$samplesize
 echo copying $PWD/Databases/BavarianCrops_$samplesize.RData to $PWD/Databases/UCR-4.RData
 cp $PWD/Databases/BavarianCrops_$samplesize.RData $PWD/Databases/UCR-4.RData
 docker run -v $PWD/Databases:/EarlyClassification/Databases -v $PWD/Results_$samplesize:/EarlyClassification/Results ec bash run.sh > $PWD/Results_$samplesize/run.log
+
+done
